d78b671f8f00f910dd155739155ec6cad4e862e2
rhdzmota <rhdzmota@gmail.com>
2022-08-31 06:33:02 -0500

    Add cli script

--- setup.py
+++ setup.py
@@ -7,14 +7,24 @@ with open("requirements.txt", "r") as file:
 with open(os.path.join("src", "rhdzmota", "VERSION"), "r") as file:
     version = file.read().strip()
 
+with open("README.md") as file:
+    readme = file.read()
+
 setup(
     name="rhdzmota",
     version=version,
+    description="RHDZMOTA Package",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url="https://github.com/rhdzmota/rhdzmota-package",
     author="Rodrigo H. Mota",
     author_email="contact@rhdzmota.com",
     package_dir={
         "": "src"
     },
+    scripts=[
+        "bin/rhdzmota"
+    ],
     install_requires=requirements,
     include_package_data=True,
 )
