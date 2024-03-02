dccfa5ef22ea571c1938d647f716f98aa5485bda
rhdzmota <contact@rhdzmota.com>
2024-03-02 09:17:44 -0600

    RHM-18 E302 E125 Stylecheck Fix

--- extensions/hello_world/src/main/rhdzmota/ext/hello_world/functions.py
+++ extensions/hello_world/src/main/rhdzmota/ext/hello_world/functions.py
@@ -3,10 +3,11 @@ from typing import Optional
 from rhdzmota.ext.hello_world.enums import Salutation
 from rhdzmota.ext.hello_world.settings import Environ
 
+
 def hello(
         recipient: Optional[str] = None,
         salutation: Optional[Salutation] = None
-    ) -> str:
+) -> str:
     recipient = recipient or Environ.EXT_HELLO_WORLD_DEFAULT_RECIPIENT.value
     salutation = salutation or Salutation.STANDARD
     return f"{salutation.value}, {recipient}!"
