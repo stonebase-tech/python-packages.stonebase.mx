09865b40e471e557e900f6198a005d37150bada8
rhdzmota <contact@rhdzmota.com>
2024-03-03 16:55:10 -0600

    RHM-19 Fix typo on cookicutter

--- extensions/streamlit_webapps/src/test/test_rhdzmota/test_ext/test_streamlit_webapps/test_settings.py
+++ extensions/streamlit_webapps/src/test/test_rhdzmota/test_ext/test_streamlit_webapps/test_settings.py
@@ -3,4 +3,4 @@ from rhdzmota.ext.streamlit_webapps.settings import Env
 
 def test_env_app_name():
     app_name = "streamlit_webapps"
-    assert Env.EXT_APP_NAME.startswith(app_name)
+    assert Env.EXT_APP_NAME.value.startswith(app_name)
