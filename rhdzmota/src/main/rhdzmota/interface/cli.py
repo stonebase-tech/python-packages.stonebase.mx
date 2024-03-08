c33e103efa9362f85f573e6505b55230a9d8e362
rhdzmota <contact@rhdzmota.com>
2024-03-08 09:47:21 -0600

    RHM-47 Update the CLIBase class with a context manager

--- rhdzmota/src/main/rhdzmota/interface/cli.py
+++ rhdzmota/src/main/rhdzmota/interface/cli.py
@@ -1,34 +1,62 @@
 import json
+import time
 import datetime as dt
 from typing import Optional
 
-from ..settings import get_environ_variable
+from rhdzmota.settings import (
+    get_environ_variable,
+    logger_manager,
+)
+
+
+logger = logger_manager.get_logger(name=__name__)
 
 
 class CLIBase:
 
     class Formatter:
 
-        def __init__(self, cls: Optional = None, default: Optional = None):  # type: ignore
+        def __init__(self, cls: Optional = None, default: Optional = None, serialize: bool = None):  # type: ignore
+            self.serialize = serialize
             self.serializer = lambda obj: json.dumps(obj, cls=cls, default=default, indent=4)
 
         def __call__(self, method):
             def wrapper(other, *args, **kwargs):
-                start = getattr(other, "start")
                 output = method(other, *args, **kwargs)
+                if not self.serialize:
+                    return output
                 if not isinstance(output, str):
                     output = self.serializer(output)
-                end = dt.datetime.utcnow()
-                print(f"\n{10*'-'} Command Output:", output, f"\n{10*'-'} Metadata:", sep="\n")
-                return {
-                    "> timestamp_start": start,
-                    "> timestamp_end": end,
-                    "> duration_seconds": (end - start).seconds,
-                }
+                return output
             return wrapper
 
-    def __init__(self):
-        self.start = dt.datetime.utcnow()
+    def __init__(
+            self,
+            extension_import_pattern: Optional[str] = None,
+        ):
+        self.extension_import_pattern = extension_import_pattern
+        self.now = dt.datetime.utcnow().isoformat()
+        self.start = time.perf_counter()
+        self.end = None
+
+    def now(self, local: bool = False) -> str:
+        return (dt.datetime.utcnow() if not local else dt.datetime.now()).isoformat()
+
+    def on_start(self):
+        logger.info("CLI Method not implemented: on_start")
+
+    def on_finalize(self):
+        logger.info("CLI method not implemented: on_finalize")
+
+    def __enter__(self) -> 'CLIBase':
+        self.on_start()
+        return self
+
+    def __exit__(self, *args):
+        self.on_finalize()
+        self.end = time.perf_counter()
+        logger.info(f"Command Start Timestamp: {self.now}")
+        logger.info(f"Command Duration: {self.end - self.start}")
 
     @Formatter()
     def environ(self, name: str) -> Optional[str]:
