7efcb65b54201889d03f8121960ce28a86554e14
rhdzmota <contact@rhdzmota.com>
2024-03-10 01:03:31 -0600

    RHM-52 Allow page to use sentry tracing by default (or deactivate if needed)

--- extensions/streamlit_webapps/src/main/rhdzmota/ext/streamlit_webapps/page_view.py
+++ extensions/streamlit_webapps/src/main/rhdzmota/ext/streamlit_webapps/page_view.py
@@ -4,6 +4,12 @@ from typing import Callable, Optional
 
 import streamlit as st
 
+from rhdzmota.settings import logger_manager
+from rhdzmota.wrappers.sentry import SentrySDKConfig
+
+
+logger = logger_manager.get_logger(name=__name__)
+
 
 class PageViewDescriptorRefname:
 
@@ -57,6 +63,7 @@ class PageView(PageViewDescriptors, PageViewSessionKeys):
     page_configs_path: Optional[str] = None
     overwrite_instance_refname: Optional[str] = None
     page_configs_kwargs: dict = field(default_factory=dict)
+    disable_sentry: bool = False
 
     @classmethod
     def inline(cls, view_name: str, func: Callable, **kwargs) -> 'PageView':
@@ -81,10 +88,25 @@ class PageView(PageViewDescriptors, PageViewSessionKeys):
             **page_configs_constructor,
         }
 
+    def enable_sentry(self):
+        # Early exit is sentry is explicitly disabled.
+        if self.disable_sentry:
+            return
+        # Configure Sentry SDK... if credentials are available.
+        session_key = "sentry_enabled"
+        if st.session_state.get(session_key) is None and SentrySDKConfig.configure_with_defaults():
+            st.session_state["sentry_enabled"] = "success"
+            logger.info("SentrySDK Correctly configured!")
+        elif st.session_state.get(session_key) is None:
+            st.session_state["sentry_enabled"] = "failure"
+            logger.warning("SentrySDK was configuration failure due to missing DNS...")
+
     def __enter__(self) -> 'PageView':
         # Page config should only be executed ONCE and
         # must be the first streamlit command.
         st.set_page_config(**self.configs_page)
+        # Sentry performance tracing
+        self.enable_sentry()
         # Register the current page in the sesion state
         st.session_state[self.session_key_page_current] = self.refname
         # Register the current page as the first page in history
