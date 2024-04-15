5fa3fa6f0c93e04e820ebcff59dc05d8faf17d32
rhdzmota <contact@rhdzmota.com>
2024-04-14 21:01:59 -0600

    RHM-81 Update the catalog with the new components (3) from the Email ACL collection

--- extensions/streamlit_webapps/src/main/rhdzmota/ext/streamlit_webapps/components/catalog.py
+++ extensions/streamlit_webapps/src/main/rhdzmota/ext/streamlit_webapps/components/catalog.py
@@ -1,10 +1,17 @@
 import enum
 
 from .comp_simple_email_access_request import SimpleEmailAccessRequest
+from .collection_email_acl_via_token.comp_email_acl_failure import PVEmailACLFailure
+from .collection_email_acl_via_token.comp_email_acl_success import PVEmailACLSuccess
+from .collection_email_acl_via_token.comp_email_acl_gatekeeper import PVEmailACLGatekeeper
 
 
 class CompCatalog(enum.Enum):
     SIMPLE_EMAIL_ACCESS_REQUEST = SimpleEmailAccessRequest
+    # Email ACL via json-web-token
+    EMAIL_ACL_FAILURE = PVEmailACLFailure
+    EMAIL_ACL_SUCCESS = PVEmailACLSuccess
+    EMAIL_ACL_GATEKEEPER = PVEmailACLGatekeeper
 
     def configure(self, **kwargs):
         return self.value(**kwargs)
